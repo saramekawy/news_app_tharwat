9501db28767f8235f607d3545e9d01f9a1f9cd33
Sara Mekawy <as1113090@gmail.com>
2023-12-01 21:19:16 +0200

    app init

---
+++ android/app/src/main/kotlin/com/example/news_app_tharwat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.news_app_tharwat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "News",
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              "Cloud",
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
