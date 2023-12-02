b7b1dd54992cda4fb617324c1338e332cad2a516
Sara Mekawy <as1113090@gmail.com>
2023-12-02 02:27:36 +0200

    article list view start

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -23,28 +23,4 @@ class CategoryCard extends StatelessWidget {
     );
   }
 }
-import 'package:flutter/material.dart';
 
-class CategoryCard extends StatelessWidget {
-  const CategoryCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 10),
-      width: 180,
-      height: 100,
-      decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(5),
-          image: const DecorationImage(
-              image: AssetImage("assets/images/science.avif"),
-              fit: BoxFit.cover)),
-      child: const Center(
-          child: Text(
-            "Technology",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          )),
-    );
-  }
-}
